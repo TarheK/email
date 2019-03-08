08e08c75c2164a6f9a57ad6b77c981bd781947d2
TarheK <42652896+TarheK@users.noreply.github.com>
2019-03-08 10:39:53 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,43 +9,43 @@ app.secret_key=os.environ["SECRET_KEY"];
 
 @app.route('/', methods=["POST","GET"])
 def renderStore():
-    if "itemsincart" not in session or "clear" in request.args:
-        session["itemsincart"] = 0
+    if "emailsincart" not in session or "clear" in request.args:
+        session["emailsincart"] = 0
         print("skrt")
-        session["item_a"] = 0
-        session["item_b"] = 0
-        session["item_c"] = 0
+        session["email_a"] = 0
+        session["email_b"] = 0
+        session["email_c"] = 0
         
         
-    return render_template("store.html",itemsincart=session["itemsincart"])
+    return render_template("store.html",emailsincart=session["emailsincart"])
     
 
 @app.route('/addToCart',methods=["POST","GET"])
 def renderAddToCart():
-    session["itemsincart"] += 1
-    if request.args["item"]=="a":
-        session["item_a"] += 1
+    session["emailsincart"] += 1
+    if request.args["email"]=="a":
+        session["email_a"] += 1
     
-    elif request.args["item"]=="b":
-        session["item_b"] += 1
+    elif request.args["email"]=="b":
+        session["email_b"] += 1
         
-    elif request.args["item"]=="c":
-        session["item_c"] += 1
-    return render_template("addToCart.html", item_name = "Item " + str(request.args["item"]))
+    elif request.args["email"]=="c":
+        session["email_c"] += 1
+    return render_template("addToCart.html", item_name = "Email " + str(request.args["email"]))
     
 @app.route('/checkout',methods=["POST","GET"])
 def renderCheckout():
-    if "itemsincart" not in session:
+    if "emailsincart" not in session:
         return redirect(url_for("renderStore"))
-    return render_template("checkout.html",itzemsincart=session["itemsincart"])
+    return render_template("checkout.html",itzemsincart=session["emailsincart"])
 
 @app.route('/bought',methods=["POST","GET"])
 def renderBought():
-    if "itemsincart" not in session:
+    if "emailsincart" not in session:
         return redirect(url_for("renderStore"))
-    items = session["itemsincart"]
-    session["itemsincart"] = 0
-    return render_template("bought.html",itemsincart=str(items))
+    items = session["emailsincart"]
+    session["emailsincart"] = 0
+    return render_template("bought.html",emailsincart=str(emails))
     
 if __name__=="__main__":
     app.run(debug=True)
