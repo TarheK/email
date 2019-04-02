7d761b9b831ec7d81c0538bb8980c02029af6a64
P3 <ortizadrian628@gmail.com>
2019-04-02 11:11:36 -0700

    it works

--- webapp.py
+++ webapp.py
@@ -2,9 +2,17 @@ import os, datetime
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
-from flask_mail import Mail 
+from flask_mail import Mail,Message 
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'ameliatarhekot@gmail.com'
+app.config['MAIL_PASSWORD'] = os.environ["password"]
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
 mail = Mail(app)
 
 app.secret_key=os.environ["SECRET_KEY"];   
@@ -46,7 +54,16 @@ def rendernext5():
     session["data5"]=request.form["data"]
     print(request.form["data"])
     print(session["data5"])
+    
+    attachData="data1,data2,data3,data4,data5\n\"%s\",\"%s\",\"%s\",\"%s\",\"%s\""%(session["data1"],session["data2"],session["data3"],session["data4"],session["data5"])
+    
+    msg = Message('Hello', sender = 'ameliatarhekot@gmail.com', recipients = ['ameliatarhekot@gmail.com'])
+    msg.attach("data.csv","text/csv",attachData)
+    
+    mail.send(msg)
+    
     return render_template("end.html")
+    
     
     
 if __name__=="__main__":
